19ff7231ec14a34f7d4ea170cdf171d7505292d5
HMisu <misuhan33@gmail.com>
2025-01-02 19:54:27 +0900

    :sparkles: feat: 공통 ApiResponse 적용

--- queue/src/main/java/com/quit/queue/application/service/QueueService.java
+++ queue/src/main/java/com/quit/queue/application/service/QueueService.java
@@ -1,14 +1,17 @@
 package com.quit.queue.application.service;
 
 import com.quit.queue.application.service.dto.res.QueueResponse;
+import com.quit.queue.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Range;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -18,7 +21,7 @@ public class QueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
     // 대기열에 사용자 추가
-    public Mono<Long> addUserToQueue(UUID storeId, Long userId) {
+    public Mono<ApiResponse<Long>> addUserToQueue(UUID storeId, Long userId) {
         String key = "queue:store:" + storeId + ":users";
         String globalUserKey = "queue:global:users"; // 모든 대기열에 존재하는 사용자 관리
 
@@ -27,56 +30,55 @@ public class QueueService {
                 .flatMap(isMember -> {
                     if (isMember) {
                         // 이미 다른 대기열에 추가되어 있는 사용자
-                        return Mono.error(new IllegalStateException("User already in another queue"));
+                        return Mono.just(ApiResponse.<Long>error(HttpStatus.BAD_REQUEST.value(), "User already in another queue"));
                     } else {
                         // 사용자가 다른 대기열에 없다면 글로벌 사용자 목록에 추가
                         return reactiveRedisTemplate.opsForSet().add(globalUserKey, userId.toString())
                                 .then(reactiveRedisTemplate.opsForValue().increment("queue:store:" + storeId + ":counter", 1)
                                         .flatMap(newScore -> reactiveRedisTemplate.opsForZSet().add(key, userId.toString(), newScore)
-                                                .thenReturn(newScore)));
+                                                .thenReturn(newScore)))
+                                .flatMap(newScore -> Mono.just(ApiResponse.success(newScore)));
                     }
                 });
     }
 
-    // store 별 대기열 조회
-    public Flux<QueueResponse> getQueue(UUID storeId) {
+    public Mono<ApiResponse<List<QueueResponse>>> getQueue(UUID storeId) {
         String key = "queue:store:" + storeId + ":users";
 
-        // storeId가 존재하는지 확인
         return reactiveRedisTemplate.hasKey(key)
-                .flatMapMany(exists -> {
+                .flatMap(exists -> {
                     if (exists) {
                         // storeId가 있을 경우 대기열 조회 후, 여러 사용자 정보를 QueueResponse에 추가
                         return reactiveRedisTemplate.opsForZSet().rangeWithScores(key, Range.closed(0L, -1L))
-                                .collectList()  // 해당 storeId에 대한 모든 대기열을 리스트로 받음
-                                .map(entries -> {
+                                .map(entry -> {
                                     QueueResponse response = new QueueResponse(storeId);
-                                    // 각 entry를 QueueResponse의 userScores 리스트에 추가
-                                    entries.forEach(entry ->
-                                            response.addUserScore(Long.valueOf(entry.getValue()), entry.getScore().floatValue()));
+                                    response.addUserScore(Long.valueOf(entry.getValue()), entry.getScore().floatValue());
                                     return response;
-                                });
+                                })
+                                .collectList()
+                                .map(ApiResponse::success);
                     } else {
                         // storeId가 없으면 전체 대기열 조회 후 QueueResponse로 변환
-                        return getAllQueues();
+                        return getAllQueues()
+                                .map(ApiResponse::success);
                     }
                 });
     }
 
-    // 전체 대기열 조회 (storeId 별로 그룹화된 결과 반환)
-    private Flux<QueueResponse> getAllQueues() {
-        // 전체 storeId를 조회하여 대기열을 반환
+    private Mono<List<QueueResponse>> getAllQueues() {
         return reactiveRedisTemplate.keys("queue:store:*:users")
-                .flatMap(key -> reactiveRedisTemplate.opsForZSet().rangeWithScores(key, Range.closed(0L, -1L))
-                        .collectList()  // 해당 storeId에 대한 모든 대기열을 리스트로 받음
-                        .map(entries -> {
-                            UUID storeId = UUID.fromString(key.split(":")[2]);
-                            QueueResponse response = new QueueResponse(storeId);
-                            // 각 entry를 QueueResponse의 userScores 리스트에 추가
-                            entries.forEach(entry ->
-                                    response.addUserScore(Long.valueOf(entry.getValue()), entry.getScore().floatValue()));
-                            return response;
-                        }));
+                .flatMap(key -> {
+                    UUID storeId = UUID.fromString(key.split(":")[2]);
+                    return reactiveRedisTemplate.opsForZSet().rangeWithScores(key, Range.closed(0L, -1L))
+                            .collectList()
+                            .map(entries -> {
+                                QueueResponse response = new QueueResponse(storeId);
+                                entries.forEach(entry ->
+                                        response.addUserScore(Long.valueOf(entry.getValue()), entry.getScore().floatValue()));
+                                return response;
+                            });
+                })
+                .collectList();
     }
 
     // 대기열에서 사용자 제거
--- queue/src/main/java/com/quit/queue/presentation/controller/QueueController.java
+++ queue/src/main/java/com/quit/queue/presentation/controller/QueueController.java
@@ -2,11 +2,13 @@ package com.quit.queue.presentation.controller;
 
 import com.quit.queue.application.service.QueueService;
 import com.quit.queue.application.service.dto.res.QueueResponse;
+import com.quit.queue.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -17,14 +19,14 @@ public class QueueController {
     private final QueueService queueService;
 
     @PostMapping("/{storeId}/users")
-    public Mono<Long> addUserToQueueForStore(@PathVariable UUID storeId,
-                                             @RequestHeader(value = "X-User-Id", required = true) Long userId) {
+    public Mono<ApiResponse<Long>> addUserToQueueForStore(@PathVariable UUID storeId,
+                                                    @RequestHeader(value = "X-User-Id", required = true) Long userId) {
         // 비동기적으로 사용자 대기열에 추가
         return queueService.addUserToQueue(storeId, userId);
     }
 
     @GetMapping
-    public Flux<QueueResponse> getQueue(@RequestParam(value = "storeId", required = false) UUID storeId) {
+    public  Mono<ApiResponse<List<QueueResponse>>> getQueue(@RequestParam(value = "storeId", required = false) UUID storeId) {
         // 비동기적으로 대기열 데이터를 가져오기
         return queueService.getQueue(storeId);
     }
