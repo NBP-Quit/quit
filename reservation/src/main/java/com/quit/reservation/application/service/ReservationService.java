1b519a70f4cd8dd4163d68c4730c1e3377021ac3
eleunadeu <eleunadeu@gmail.com>
2025-01-02 17:36:01 +0900

    :sparkles: feat: 예약 정보 생성 데이터 검증 기능 구현 - 예약 정보 입력 값 검증 코드 추가

--- reservation/src/main/java/com/quit/reservation/application/service/ReservationService.java
+++ reservation/src/main/java/com/quit/reservation/application/service/ReservationService.java
@@ -5,11 +5,13 @@ import com.quit.reservation.application.dto.CreateReservationResponse;
 import com.quit.reservation.domain.enums.ReservationStatus;
 import com.quit.reservation.domain.model.Reservation;
 import com.quit.reservation.domain.repository.ReservationRepository;
-import com.quit.reservation.presentation.request.CreateReservationRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Service
 @Slf4j
@@ -30,6 +32,9 @@ public class ReservationService {
         log.info("예약 생성 작업 시작");
         //TODO: Store application에서 예약 관련 정보 feign client로 받아오기
 
+        //TODO: 데이터 검증 작업 : 추후 Store 정보 가져와서 비교해서 검증하는 것으로 변경
+        validateReservationRequest(request);
+
         Reservation reservation = reservationRepository.save(
                 Reservation.create(customerId, request.getStoreId(),
                         request.getGuestCount(), request.getReservationDate(), request.getReservationTime(),
@@ -41,5 +46,28 @@ public class ReservationService {
         log.info("예약 UUID : {}", reservation.getReservationId());
         log.info("예약 정보 생성 완료");
         return CreateReservationResponse.of(reservation.getReservationId());
+    }
+
+    private void validateReservationRequest(CreateReservationDto request) {
+        if (request.getStoreId() == null || request.getGuestCount() == null ||
+                request.getReservationDate() == null || request.getReservationTime() == null) {
+            throw new IllegalArgumentException("필수 입력 데이터가 누락되었습니다.");
+        }
+
+        if (request.getGuestCount() <= 0) {
+            throw new IllegalArgumentException("예약 인원은 최소 1명 이상이어야 합니다.");
+        }
+
+        LocalDate today = LocalDate.now();
+        LocalDate reservationDate = request.getReservationDate();
+        if (reservationDate.isBefore(today) || reservationDate.isEqual(today)) {
+            throw new IllegalArgumentException("예약할 수 없는 예약 날짜 입니다.");
+        }
+
+        LocalTime reservationTime = request.getReservationTime();
+        if (reservationTime.isBefore(LocalTime.of(0, 0))
+                || reservationTime.isAfter(LocalTime.of(23, 59))) {
+            throw new IllegalArgumentException("예약할 수 없는 예약 시간 입니다.");
+        }
     }
 }
