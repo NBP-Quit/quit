0e198a54286236d04cd8f17cb6d9e70a4e60e969
sohyuneeee <sohyuney@naver.com>
2025-01-04 18:17:32 +0900

    :sparkles: feat: ErrorType 에 예약 스케줄 수정 관련 예외처리 정의

--- store/src/main/java/com/quit/store/presentation/exception/ErrorType.java
+++ store/src/main/java/com/quit/store/presentation/exception/ErrorType.java
@@ -26,6 +26,12 @@ public enum ErrorType {
     STORE_CLOSE_TIME_EMPTY(BAD_REQUEST, "가게 마감 시간이 존재하지 않습니다."),
     STORE_LAST_ORDER_TIME_EMPTY(BAD_REQUEST, "가게 주문 마감 시간이 존재하지 않습니다."),
     STORE_CATEGORY_EMPTY(BAD_REQUEST, "가게 카테고리가 존재하지 않습니다."),
+
+    RESERVATION_SLOT_NOT_FOUND(NOT_FOUND, "예약 스케줄이 존재하지 않습니다."),
+    RESERVATION_SLOT_DATE_CHANGE_NOT_ALLOWED(BAD_REQUEST, "현재 예약된 인원이 있어 날짜를 변경할 수 없습니다."),
+    RESERVATION_SLOT_TIME_CHANGE_NOT_ALLOWED(BAD_REQUEST, "현재 예약된 인원이 있어 시간을 변경할 수 없습니다."),
+    RESERVATION_SLOT_MAX_CAPACITY_INVALID(BAD_REQUEST, "최대 예약 가능 인원이 현재 예약된 인원보다 적을 수 없습니다."),
+
     ;
 
     private final HttpStatus httpStatus;
