b5aa4cf5a1fa3334be6629998e8ce5c6c7875080
geon8692 <geon4735@gmail.com>
2025-01-05 23:19:52 +0900

    :sparkles: 이미지 생성 기능 구현

--- review/src/main/java/com/quit/review/domain/model/Image.java
+++ review/src/main/java/com/quit/review/domain/model/Image.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 import org.hibernate.annotations.SQLRestriction;
 
+import com.quit.review.common.BaseEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -14,15 +16,17 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "p_image")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @SQLRestriction("is_deleted is false")
-public class Image {
+public class Image extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
@@ -30,20 +34,43 @@ public class Image {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(nullable = false)
+	@Setter
 	private Review review;
 
 	@Column(nullable = false)
 	private String url;
 
 	@Column(nullable = false)
-	private String fileName;
+	private String filename;
 
 	@Column(nullable = false)
-	private String originalFileName;
+	private String originalFilename;
 
 	@Column(nullable = false)
 	private String contentType;
 
 	@Column(nullable = false)
 	private Long size;
+
+	@Builder
+	private Image(UUID id, Review review, String url, String filename, String originalFilename, String contentType,
+		Long size) {
+		this.id = id;
+		this.review = review;
+		this.url = url;
+		this.filename = filename;
+		this.originalFilename = originalFilename;
+		this.contentType = contentType;
+		this.size = size;
+	}
+
+	public static Image create(String url, String filename, String originalFilename, String contentType, Long size) {
+		return Image.builder()
+			.url(url)
+			.filename(filename)
+			.originalFilename(originalFilename)
+			.contentType(contentType)
+			.size(size)
+			.build();
+	}
 }
