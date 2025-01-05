d836511b5368d8fab03263c211c94101663567d2
geon8692 <geon4735@gmail.com>
2025-01-06 00:21:01 +0900

    :adhesive_bandage: fix: @AllArgsConstructor 추가

--- review/src/main/java/com/quit/review/presentation/request/ScoresRequest.java
+++ review/src/main/java/com/quit/review/presentation/request/ScoresRequest.java
@@ -3,11 +3,13 @@ package com.quit.review.presentation.request;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class ScoresRequest {
 
 	@Min(1) @Max(5)
