f636465999361ae2a86dcbd13f0b339978d654ee
eleunadeu <eleunadeu@gmail.com>
2025-01-06 11:54:16 +0900

    :sparkles: feat: GlobalExceptionHandler 추가

---
+++ reservation/src/main/java/com/quit/reservation/presentation/exception/error/ErrorType.java
@@ -0,0 +1,22 @@
+package com.quit.reservation.presentation.exception.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorType {
+
+    COMMON_INVALID_PARAMETER( BAD_REQUEST, "잘못된 파라미터입니다."),
+    COMMON_SERVER_ERROR(INTERNAL_SERVER_ERROR, "서버에서 에러가 발생하였습니다."),
+    COMMON_VALIDATION_ERROR(BAD_REQUEST, "요청 데이터가 유효하지 않습니다."),
+    USER_NOT_SAME(BAD_REQUEST, "해당 작성자가 아닙니다."),
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
