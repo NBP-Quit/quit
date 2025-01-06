044eb7563e35165e1b0ad4ccc44314d46a8bcd5f
sohyuneeee <sohyuney@naver.com>
2025-01-06 18:51:38 +0900

    :sparkles: feat: ErrorType 추가

--- payment/src/main/java/com/quit/payment/presentation/exception/ErrorType.java
+++ payment/src/main/java/com/quit/payment/presentation/exception/ErrorType.java
@@ -10,9 +10,11 @@ import static org.springframework.http.HttpStatus.*;
 @AllArgsConstructor
 public enum ErrorType {
 
-    COMMON_INVALID_PARAMETER( BAD_REQUEST, "잘못된 파라미터입니다."),
+    COMMON_INVALID_PARAMETER(BAD_REQUEST, "잘못된 파라미터입니다."),
     COMMON_SERVER_ERROR(INTERNAL_SERVER_ERROR, "서버에서 에러가 발생하였습니다."),
     COMMON_VALIDATION_ERROR(BAD_REQUEST, "요청 데이터가 유효하지 않습니다."),
+
+    PAYMENT_DATA_INVALID(NOT_FOUND, "해당 결제 데이터가 존재하지 않습니다."),
 
     ;
 
