b6f7238963219fd7877671d6f9f23bc88b218cb9
eleunadeu <eleunadeu@gmail.com>
2025-01-07 11:57:20 +0900

    :recycle: refactor: ReservationJpaRepository 수정 - reservation repository의 메서드를 jpa Repository에서 override

--- reservation/src/main/java/com/quit/reservation/infrastructure/repository/ReservationJpaRepository.java
+++ reservation/src/main/java/com/quit/reservation/infrastructure/repository/ReservationJpaRepository.java
@@ -1,10 +1,15 @@
 package com.quit.reservation.infrastructure.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.StringPath;
+import com.quit.reservation.domain.enums.Role;
 import com.quit.reservation.domain.model.QReservation;
 import com.quit.reservation.domain.model.Reservation;
+import com.quit.reservation.domain.repository.ReservationRepository;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
@@ -12,7 +17,10 @@ import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 import java.util.*;
 
+import static com.quit.reservation.domain.model.QReservation.reservation;
+
 public interface ReservationJpaRepository extends JpaRepository<Reservation, UUID>,
+        ReservationRepository,
         QuerydslPredicateExecutor<Reservation>,
         QuerydslBinderCustomizer<QReservation> {
 
@@ -29,5 +37,36 @@ public interface ReservationJpaRepository extends JpaRepository<Reservation, UUI
             }
             return Optional.of(booleanBuilder);
         });
+    }
+
+    @Override
+    default Optional<Reservation> findByReservationIdIsDeletedFalse(UUID reservationId) {
+        return findOne(
+                reservation.isDeleted.eq(false).and(reservation.reservationId.eq(reservationId))
+        );
+    }
+
+
+    @Override
+    default Page<Reservation> findReservationsByUser(String customerId, Pageable pageable) {
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(reservation.isDeleted.eq(false));
+        builder.and(reservation.customerId.eq(customerId));
+        return findAll(builder, pageable);
+    }
+
+    @Override
+    default Page<Reservation> findReservationsByStore(UUID storeId, Pageable pageable) {
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(reservation.isDeleted.eq(false));
+        builder.and(reservation.storeId.eq(storeId));
+        return findAll(builder, pageable);
+    }
+
+    @Override
+    default Page<Reservation> findAllReservations(Role role, Predicate predicate, Pageable pageable) {
+        BooleanBuilder builder = new BooleanBuilder(predicate);
+        builder.and(reservation.isDeleted.eq(false));
+        return findAll(builder, pageable);
     }
 }
