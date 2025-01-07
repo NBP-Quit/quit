881bb5fd617cf15b9efe4b5ecca62b8894f223fe
HMisu <misuhan33@gmail.com>
2025-01-07 16:32:24 +0900

    :sparkles: feat: 스케줄 사용을 위한 @EnableScheduling 추가

--- queue/src/main/java/com/quit/queue/QueueApplication.java
+++ queue/src/main/java/com/quit/queue/QueueApplication.java
@@ -2,7 +2,9 @@ package com.quit.queue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class QueueApplication {
 
