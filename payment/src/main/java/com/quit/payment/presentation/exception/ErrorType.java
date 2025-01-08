89b92e56adc67ade3dcf5c22c395eec8ad97e61a
sohyuneeee <sohyuney@naver.com>
2025-01-08 20:30:37 +0900

    :sparkles: feat: 예약 단 건 조회 실패 ErrorType 정의

--- payment/src/main/java/com/quit/payment/presentation/exception/ErrorType.java
+++ payment/src/main/java/com/quit/payment/presentation/exception/ErrorType.java
@@ -16,6 +16,8 @@ public enum ErrorType {
 
     PAYMENT_DATA_INVALID(NOT_FOUND, "해당 결제 데이터가 존재하지 않습니다."),
     PAYMENT_NOT_FOUND(NOT_FOUND, "결제가 존재하지 않습니다."),
+
+    RESERVATION_ID_MISMATCH(BAD_REQUEST, "결제 데이터와 예약 ID가 일치하지 않습니다.")
     ;
 
     private final HttpStatus httpStatus;
