441b68ddf83571d4150cdd152bed4c9db368deb2
HMisu <misuhan33@gmail.com>
2025-01-08 19:03:57 +0900

    :sparkles: feat: MASTER만 접근 가능하도록 filter 추가

---
+++ queue/src/main/java/com/quit/queue/infrastructure/filter/PrometheusAuthorizationFilter.java
@@ -0,0 +1,34 @@
+package com.quit.queue.infrastructure.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class PrometheusAuthorizationFilter implements WebFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+
+        if (path.equals("/actuator/prometheus")) {
+            String userAgent = exchange.getRequest().getHeaders().getFirst("User-Agent");
+
+            if (userAgent != null && userAgent.contains("Prometheus")) {
+                return chain.filter(exchange);
+            }
+
+            String headerValue = exchange.getRequest().getHeaders().getFirst("X-User-Role");
+
+            if (headerValue == null || !headerValue.equals("MASTER")) {
+                return Mono.error(new RuntimeException("Unauthorized"));
+            }
+        }
+
+        return chain.filter(exchange);
+    }
+}
