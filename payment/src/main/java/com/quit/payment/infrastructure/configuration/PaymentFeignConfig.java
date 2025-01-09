f4a33f22e32a411269760e6a0f809aa2b263d322
sohyuneeee <sohyuney@naver.com>
2025-01-09 19:44:06 +0900

    :sparkles: feat: feignconfig 적용

--- payment/src/main/java/com/quit/payment/infrastructure/configuration/PaymentFeignConfig.java
+++ payment/src/main/java/com/quit/payment/infrastructure/configuration/PaymentFeignConfig.java
@@ -1,16 +1,9 @@
 package com.quit.payment.infrastructure.configuration;
 
-import feign.Logger;
-import feign.Request;
 import feign.RequestInterceptor;
-import feign.Retryer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.TimeUnit;
-
-@Configuration
 @RequiredArgsConstructor
 public class PaymentFeignConfig {
 
@@ -19,21 +12,6 @@ public class PaymentFeignConfig {
     @Bean
     public RequestInterceptor paymentAuthInterceptor() {
         return new PaymentAuthInterceptor(paymentProperties);
-    }
-
-    @Bean
-    public Request.Options requestOptions() {
-        return new Request.Options(3, TimeUnit.SECONDS, 30, TimeUnit.SECONDS, true);
-    }
-
-    @Bean
-    public Retryer retryer() {
-        return new Retryer.Default(2000, 5000, 3);
-    }
-
-    @Bean
-    Logger.Level feignLoggerLevel() {
-        return Logger.Level.FULL;
     }
 
 }
