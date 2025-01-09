4e953af774a1ba1a973f1a86f1ede0e5ba80c478
laira2 <laira2@gachon.ac.kr>
2025-01-09 15:19:06 +0900

    :sparkles: feat: rate limiter 적용

---
+++ gateway-server/src/main/java/com/quit/gateway/infrastructure/config/RateLimitConfig.java
@@ -0,0 +1,23 @@
+package com.quit.gateway.infrastructure.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimitConfig {
+
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> {
+            // 로그인 URL일 때만 IP 주소를 키 값으로 사용
+            if (exchange.getRequest().getPath().value().equals("/api/auth/login")) {
+                String ipAddress = exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
+                return Mono.just(ipAddress);
+            }
+            return Mono.empty(); // 기본 키 값 설정 (필요한 경우)
+        };
+    }
+}
+
