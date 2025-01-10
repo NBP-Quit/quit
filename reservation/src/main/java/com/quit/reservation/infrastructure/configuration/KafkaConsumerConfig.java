108dd3b6c828670f47292a4069134ff5c4065caa
eleunadeu <eleunadeu@gmail.com>
2025-01-10 18:17:21 +0900

    :bug: fix: kafka consumer 설정 수정 - 역직렬화 문제 해결을 위한 kafka consumer 설정 수정

--- reservation/src/main/java/com/quit/reservation/infrastructure/configuration/KafkaConsumerConfig.java
+++ reservation/src/main/java/com/quit/reservation/infrastructure/configuration/KafkaConsumerConfig.java
@@ -1,5 +1,7 @@
 package com.quit.reservation.infrastructure.configuration;
 
+import com.quit.reservation.infrastructure.messaging.message.PaymentMessage;
+import com.quit.reservation.infrastructure.messaging.message.ReservationMessage;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,25 +20,51 @@ public class KafkaConsumerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
+
     @Value("${spring.kafka.consumer.auto-offset-reset}")
     private String autoOffsetReset;
 
-    @Bean
-    public ConsumerFactory<String, Object> consumerFactory() {
+    private Map<String, Object> commonKafkaConfig() {
         Map<String, Object> kafkaConfig = new HashMap<>();
         kafkaConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         kafkaConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
         kafkaConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         kafkaConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         kafkaConfig.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        return new DefaultKafkaConsumerFactory<>(kafkaConfig);
+        kafkaConfig.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+        return kafkaConfig;
+    }
+
+    private <T> ConsumerFactory<String, T> createConsumerFactory(Class<T> valueType) {
+        Map<String, Object> kafkaConfig = commonKafkaConfig();
+        kafkaConfig.put(JsonDeserializer.VALUE_DEFAULT_TYPE, valueType.getName());
+        return new DefaultKafkaConsumerFactory<>(kafkaConfig, new StringDeserializer(), new JsonDeserializer<>(valueType));
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
+    public ConsumerFactory<String, PaymentMessage> paymentConsumerFactory() {
+        return createConsumerFactory(PaymentMessage.class);
+    }
+
+    @Bean
+    public ConsumerFactory<String, ReservationMessage> queueConsumerFactory() {
+        return createConsumerFactory(ReservationMessage.class);
+    }
+
+    private <T> ConcurrentKafkaListenerContainerFactory<String, T> createKafkaListenerContainerFactory(ConsumerFactory<String, T> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, T> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
         return factory;
     }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PaymentMessage> paymentKafkaListenerContainerFactory() {
+        return createKafkaListenerContainerFactory(paymentConsumerFactory());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, ReservationMessage> queueKafkaListenerContainerFactory() {
+        return createKafkaListenerContainerFactory(queueConsumerFactory());
+    }
 }
+
