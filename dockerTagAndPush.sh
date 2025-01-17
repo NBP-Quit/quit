8a5a2daa075f173ad105780c97e642c581aa6684
laira2 <laira2@gachon.ac.kr>
2025-01-17 11:29:20 +0900

    :construction_worker: ci: 이미지 위치 재확인

--- dockerTagAndPush.sh
+++ dockerTagAndPush.sh
@@ -2,7 +2,7 @@
 
 # 서비스 리스트
 services=(
-  "eureka" "gateway" "payment" "queue"
+  "eureka-server" "gateway-server" "payment" "queue"
   "reservation" "review" "store" "user"
 )
 
@@ -15,28 +15,14 @@ do
   latestTag="$service-latest"  # latest 태그
   echo "======= 현재 위치 : $(pwd) ======="
   echo "======= 확인 중인 서비스 : $service ======="
+  # 현재 디렉토리 확인
+  echo "현재 경로 확인: $(pwd)"
 
- # 디렉토리와 파일 확인
-  servicePath="/home/runner/work/quit/quit/$service"
-  echo "=== 디렉토리 내용 확인: $servicePath ==="
-  if [ -d "$servicePath" ]; then
-    ls -l "$servicePath"
-  else
-    echo "$servicePath 디렉토리가 존재하지 않습니다."
-  fi
+  # 현재 디렉토리 내 파일 목록 확인
+  echo "현재 경로 내 파일 목록:"
+  ls -l
 
-  # eureka 디렉토리 검색
-  if [ "$service" == "eureka" ]; then
-    echo "=== 'eureka' 디렉토리 위치 검색 ==="
-    find / -type d -name "eureka" 2>/dev/null
-  fi
-
-  # 도커 이미지 빌드 (각 서비스 디렉토리에 Dockerfile이 있어야 함)
-  if [ -d "$servicePath" ]; then
-    docker build -t "$imageName:$latestTag" "$servicePath"
-  else
-    echo "$servicePath 경로가 잘못되었거나 디렉토리가 없습니다."
-  fi
+  docker build -t "$imageName:$latestTag" "/home/runner/work/quit/quit/$service"
 
   # Docker Hub에 푸시
   docker push "$imageName:$latestTag"
