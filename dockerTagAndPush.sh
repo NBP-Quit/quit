4d582d37ccd56521d07a138bca7234b62b9d8c21
laira2 <laira2@gachon.ac.kr>
2025-01-17 11:22:33 +0900

    :construction_worker: ci: 이미지 위치 재확인

--- dockerTagAndPush.sh
+++ dockerTagAndPush.sh
@@ -16,11 +16,27 @@ do
   echo "======= 현재 위치 : $(pwd) ======="
   echo "======= 확인 중인 서비스 : $service ======="
 
-  # 디렉토리와 파일 확인
-  echo "=== 디렉토리 내용 확인: /home/runner/work/quit/quit/$service ==="
-  ls -l "/home/runner/work/quit/quit/$service" || echo "디렉토리가 존재하지 않습니다."
+ # 디렉토리와 파일 확인
+  servicePath="/home/runner/work/quit/quit/$service"
+  echo "=== 디렉토리 내용 확인: $servicePath ==="
+  if [ -d "$servicePath" ]; then
+    ls -l "$servicePath"
+  else
+    echo "$servicePath 디렉토리가 존재하지 않습니다."
+  fi
 
-  docker build -t "$imageName:$latestTag" "/home/runner/work/quit/quit/$service"
+  # eureka 디렉토리 검색
+  if [ "$service" == "eureka" ]; then
+    echo "=== 'eureka' 디렉토리 위치 검색 ==="
+    find / -type d -name "eureka" 2>/dev/null
+  fi
+
+  # 도커 이미지 빌드 (각 서비스 디렉토리에 Dockerfile이 있어야 함)
+  if [ -d "$servicePath" ]; then
+    docker build -t "$imageName:$latestTag" "$servicePath"
+  else
+    echo "$servicePath 경로가 잘못되었거나 디렉토리가 없습니다."
+  fi
 
   # Docker Hub에 푸시
   docker push "$imageName:$latestTag"
