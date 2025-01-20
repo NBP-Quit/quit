0e5ecf742c4337f8a6e96229327915fb017442e3
sohyuneeee <sohyuney@naver.com>
2025-01-21 00:39:47 +0900

    :sparkles: feat: 멱등키 redis 저장 및 조회 관련 service 생성

---
+++ payment/src/main/java/com/quit/payment/application/service/IdempotencyService.java
@@ -0,0 +1,31 @@
+package com.quit.payment.application.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class IdempotencyService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void saveIdempotencyKey(String idempotencyKey, String paymentKey) {
+        log.info(">>>>> redis 저장 <<<<<");
+        redisTemplate.opsForValue().set(
+                "idempotencyKey::" + idempotencyKey,
+                paymentKey,
+                Duration.ofDays(15) // 15일간 유효
+        );
+    }
+
+    public String getPaymentKeyByIdempotencyKey(String idempotencyKey) {
+        log.info(">>>>> redis 조회 <<<<<");
+        return redisTemplate.opsForValue().get("idempotencyKey::" + idempotencyKey);
+    }
+
+}
